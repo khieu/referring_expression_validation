0451ef6ee3ea9631fbca9abf34c612e43d102ef6
hieule <hieule246@gmail.com>
2021-03-17 18:32:54 +0700

    update front-end

--- download_sample_data.py
+++ download_sample_data.py
@@ -1,9 +1,13 @@
 import subprocess
 import pandas as pd
 
-file_id = 100
-subprocess.run([f'scp pc7:/scratch2/hle/refCOCO/train/attr_tables/attr_{file_id}.tsv ./'], shell=True)
-df = pd.read_csv(f'./attr_{file_id}.tsv', encoding='utf-8',sep='\t')
-img_id = df['image_id'][0]
-subprocess.run([f'scp pc7:/scratch2/hle/refCOCO/train/imgs_by_id/{img_id}.jpg ./'], shell=True)
+
+for file_id in range(20):
+	#file_id = 200
+
+	#subprocess.run([f'scp pc7:/scratch2/hle/refCOCO/test/attr_tables/attr_{file_id}.tsv ./'], shell=True)
+	#subprocess.run([f'scp pc7:/scratch2/hle/refCOCO/test/labels/lab_{file_id}.json ./'], shell=True)
+	#df = pd.read_csv(f'./attr_{file_id}.tsv', encoding='utf-8',sep='\t')
+	#img_id = df['image_id'][0]
+	subprocess.run([f'scp pc7:/scratch2/hle/git/rsa_refer/data/validation_images/test/validation_img_{file_id}.png ./'], shell=True)
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as Papa from 'papaparse';
+const axios = require('axios').default;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: 'None',
+      choice: ''
+    };
+  }
+
+  onValueChange = (event) => {
+    this.setState({choice: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    if (this.state.choice == '') {
+      event.preventDefault();
+    } else {
+      let file_name = this.state.image.split('/').reverse()[0];
+      let img_id = file_name.substring(0, file_name.indexOf('.'));
+      axios({
+        url: 'http://localhost:3000/submit',
+        method: 'post',
+        params:{
+          'img_id' : img_id,
+          response: this.state.choice 
+        }
+        ,headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+      })
+      .then( (response) => {
+        console.log(response);
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    }
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/')
+    .then( (response) => {
+      this.setState({image: response.data});
+    })
+    .catch(function(err){
+      console.log(err)
+    });
+  }
+  render() {
+    console.log(this.state.image);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Evaluating referring expression
+          </p>
+        </header>
+        <img src={this.state.image} />
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='0'} value="0" onChange={this.onValueChange}/>
+          0
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='1'} value="1" onChange={this.onValueChange}/>
+          1
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='2'} value="2" onChange={this.onValueChange}/>
+          2
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='3'} value="3" onChange={this.onValueChange}/>
+          3
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='4'} value="4" onChange={this.onValueChange}/>
+          4
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='5'} value="5" onChange={this.onValueChange}/>
+          5
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='no_match'} value="no_match" onChange={this.onValueChange}/>
+          there is nothing in the picture matching this description
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='underinformative'} value="underinformative" onChange={this.onValueChange}/>
+          there are several things that match this description equally well
+        </label><br/>
+        <label>
+          <input type="radio" name="name" checked={this.state.choice=='not_highlighted'} value="not_highlighted" onChange={this.onValueChange}/>
+          the thing that matches the description best is not highlighted 
+        </label><br/>
+        <input type="submit" value="Submit" />
+      </form>
+      <br/>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
